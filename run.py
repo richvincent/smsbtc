1977621a2780d8941f2b0d5e92e6c67fc9bc92d4
Richard Vincent <rich@richardvincent.com>
2016-10-15 01:15:47 -0400

    Modified run.py with port

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect
 import twilio.twiml
+import os
 
 app = Flask(__name__)
 
@@ -12,4 +13,6 @@ def hello_monkey():
     resp.message("Hello, Mobile Monkey")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Bind to Port if defined, otherwise default to 5000
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
