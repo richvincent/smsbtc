36629e11433791c30c504be753d15d1b49244105
Richard Vincent <rich@richardvincent.com>
2016-10-15 13:24:01 -0400

    fixed run.py issue. no return issue.

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ def hello_monkey():
 
     resp = twilio.twiml.Response()
     resp.message("Hello, Mobile Monkey")
+    return str(resp)
 
 if __name__ == "__main__":
     # Bind to Port if defined, otherwise default to 5000
