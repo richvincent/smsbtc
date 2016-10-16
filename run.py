048c9102a334fd4c5892c867028798d0a12276f5
Richard Vincent <rich@richardvincent.com>
2016-10-16 01:29:21 -0400

    request object update

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def sms():
     number = request.form['From']
-    message_body = request._form['Body']
+    message_body = request.form['Body']
 
     resp = twilio.twiml.Response()
     resp.message('Hello {}, you said: {}'.format(number, message_body))
