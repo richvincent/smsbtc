d4a8a72d7c681505a5982c80a530f1134a72130e
Richard Vincent <rich@richardvincent.com>
2016-10-16 01:09:39 -0400

    Working with request objects

--- run.py
+++ run.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=['GET', 'POST'])
-def hello_monkey():
-    """Respond to incoming calls with a simple test message."""
+def sms():
+    number = request.form['From']
+    message_body = request._form['Body']
 
     resp = twilio.twiml.Response()
-    resp.message("SMS Blockchain System")
+    resp.message('Hello {}, you said: {}'.format(number, message_body))
     return str(resp)
 
 if __name__ == "__main__":
